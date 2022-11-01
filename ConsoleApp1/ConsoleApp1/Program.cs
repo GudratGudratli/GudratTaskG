c95b25fe6a4db4fac45ef66701e252b3b3284a58
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2022-11-01 23:47:04 +0400

    Task: arrays

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int[] array = { 1, 15, 35, 7, 46, 0, 17, 44, 8 };
+var count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 0 && array[i] <= 9)
+    {
+        count++;
+    }
+}
+Console.WriteLine("{" + array + "}" + " arrayında " + count + " rəqəm var.");
