b8a8935ab5b6574607638536e404412b1de5439a
orxan353654574244556 <tu7jsvzdx@code.edu.az>
2022-11-02 08:03:49 +0400

    Modified

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-int[] array = { 1, 15, 35, 7, 46, 0, 17, 44, 8 };
-var count = 0;
-for (int i = 0; i < array.Length; i++)
+
+void Task2()
 {
-    if (array[i] >= 0 && array[i] <= 9)
+    int[] array = { 1, 15, 35, 7, 46, 0, 17, 44, 8 };
+    var count = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        count++;
+        if (array[i] >= 0 && array[i] <= 9)
+        {
+            count++;
+        }
     }
+    Console.WriteLine("{" + array + "}" + " arrayında " + count + " rəqəm var.");
 }
-Console.WriteLine("{" + array + "}" + " arrayında " + count + " rəqəm var.");
+
+void Task1()
+{
+    int i = 0;
+    int small = 0;
+    int[] arr = { 5, 8, 2, 15, 84 };
+
+    small = arr[0];
+
+    for (i = 1; i < arr.Length; i++)
+    {
+
+        if (small > arr[i])
+            small = arr[i];
+    }
+
+    Console.WriteLine("Smallest element in array: " + small);
+
+}
+Task1();
